0de0cc6efcb43f21bd1d5496185bcc949b097e85
SamraAzizi <samarraazizi@gmail.com>
2025-05-20 16:39:45 +0500

    web sraping of price tracker

--- backend/app.py
+++ backend/app.py
@@ -7,3 +7,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+
+class ProductResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    img = db.Column(db.String(1000))
+    url = db.Column(db.String(1000))
+    price = db.Column(db.Float)
