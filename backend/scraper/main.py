32ba9fdabe13c08bbd84ca69e07cf44b6a46bcdd
SamraAzizi <samarraazizi@gmail.com>
2025-05-25 18:13:54 +0500

    web sraping of price tracker

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -4,3 +4,15 @@ import json
 import os
 from amazon import get_product as get_amazon_product
 from requests import post
+
+AMAZON = "https://amazon.ca"
+
+URLS = {
+    AMAZON: {
+        "search_field_query": 'input[name="field-keywords"]',
+        "search_button_query": 'input[value="Go"]',
+        "product_selector": "div.s-card-container"
+    }
+}
+
+available_urls = URLS.keys()
