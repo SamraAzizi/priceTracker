2bf359686ece0e43c0ae39beebf082036037d31d
SamraAzizi <samarraazizi@gmail.com>
2025-05-29 21:19:08 +0500

    web sraping of price tracker frontend

--- frontend/my-app/src/components/TrackedProductList.jsx
+++ frontend/my-app/src/components/TrackedProductList.jsx
@@ -14,3 +14,22 @@ const TrackedProductList = () => {
       const response = await axios.get(
         "http://localhost:5000/tracked-products"
       );
+
+      setTrackedProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching tracked products:", error);
+    }
+  };
+
+  const handleNewTrackedProductChange = (event) => {
+    setNewTrackedProduct(event.target.value);
+  };
+
+  const handleAddTrackedProduct = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/add-tracked-product",
+        {
+          name: newTrackedProduct,
+        }
+      );
